5eb158b97d84ff070e1eafa3f787e36c1ee4e791
Loic2dot0 <moreaux.loic@gmail.com>
2023-02-27 23:21:37 +0100

    création du composant TheMenu

---
+++ src/components/TheMenu.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+    </v-list>
+</template>
+
+<script>
+export default {
+    name: "TheMenu"
+}
+
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
