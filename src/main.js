d95447419e8ca30cfc48f7bc29f09dbdae13d0d1
Loic2dot0 <moreaux.loic@gmail.com>
2023-02-27 23:20:21 +0100

    ajout de la bibliothèque d'icone

--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' 
+
+export default createVuetify({
+    icons: {
+      defaultSet: 'mdi'
+    }
+  })
 
 //import './assets/main.css'
 
