6f9a828719d00882b8bfa06969567d44a5568489
Loic2dot0 <moreaux.loic@gmail.com>
2023-02-28 15:13:03 +0100

    Modification du template principal suppression de fichiers inutilisés

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
-  <v-card>
-    <v-layout>
-      <v-navigation-drawer>
-        <TheMenu></TheMenu>
+    <v-app>
+      <v-navigation-drawer v-model="drawer">
+        <Menu></Menu>
       </v-navigation-drawer>
 
       <v-app-bar :elevation="2" >
-        <v-app-bar-nav-icon><v-icon icon="mdi-menu" /></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon @click="drawer = !drawer" ></v-app-bar-nav-icon>
         <v-app-bar-title>Vue Tracker</v-app-bar-title>
       </v-app-bar>
 
-      <v-main></v-main>
-    </v-layout>
-  </v-card>
+      <v-main>
+
+      </v-main>
+    </v-app>
 </template>
 
-<script setup>
-import TheMenu from "./components/TheMenu.vue";
+<script>
+import Menu from "./views/Menu.vue";
 
+export default {
+  data(){
+    return {
+      drawer: null
+    } 
+  }
+}
 </script>
 
 <style scoped>
