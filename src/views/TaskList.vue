9f3382890bf2c1f853603421c69cb0fa3635e180
Loic2dot0 <moreaux.loic@gmail.com>
2023-03-04 12:24:14 +0100

    création de TaskList Module de gestion de l'affichage des tâches

---
+++ src/views/TaskList.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-table>
+        <thead>
+            <tr>
+                <th class="text-left">
+                    Tâche
+                </th>
+                <th class="text-left">
+                    Début - fin
+                </th>
+                <th class="text-left">
+                    Durée
+                </th>
+                <th class="text-left">
+                    Actions
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr
+                v-for="task in tasks"
+                :key="task.id"
+            >
+                <td>{{ task.name }}</td>
+                <td>{{ task.startTime }} - {{ task.endTime }}</td>
+                <td>00:00:00</td>
+                <td>action button</td>
+            </tr>
+        </tbody>
+    </v-table>
+</template>
+
+<script>
+    export default {
+        name: "TaskList",
+        props: {
+            tasks: {
+                type: Array,
+                default: []
+            }
+        },
+        data(){
+            return {
+         
+            } 
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
