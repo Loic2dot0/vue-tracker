a260bba288d7898bf0f3954005ac6b96d3733066
Loic2dot0 <moreaux.loic@gmail.com>
2023-03-09 20:07:12 +0100

    correctif d'une erreur de syntaxe modification de nom de constante

--- src/services/TaskService.js
+++ src/services/TaskService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const JSON_BIN_SECRET = inport.meta.env.VITE_JSON_BIN_SECRET;
-const JSON_BIN_KEY_ID = inport.meta.env.VITE_JSON_BIN_KEY_ID;
+const JSON_BIN_SECRET = import.meta.env.VITE_JSON_BIN_SECRET;
+const JSON_BIN_ID = import.meta.env.VITE_JSON_BIN_ID;
 
 const instance = axios.create({
-    baseURL: `https://api.jsonbin.io/v3/b/${JSON_BIN_KEY_ID}`,
+    baseURL: `https://api.jsonbin.io/v3/b/${JSON_BIN_ID}`,
     headers: {
         "X-Access-Key": JSON_BIN_SECRET,
     }
